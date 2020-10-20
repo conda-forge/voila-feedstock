e885494eac2b5b7ff6bd0dd655b8275d3cb8ef8c
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-10-20 17:07:37 +0200

    just use pip

--- recipe/install_sourcemaps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-mkdir -p $PREFIX/share/jupyter/voila/templates/base/static
-cp $SRC_DIR/voila-sourcemaps/share/jupyter/voila/templates/base/static/*.map $PREFIX/share/jupyter/voila/templates/base/static
