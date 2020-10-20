8971e068ee92a7479141afa8289272d97f226eeb
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-10-20 13:46:26 +0200

    build in place

--- recipe/install_sourcemaps.sh
+++ recipe/install_sourcemaps.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail
 
+# build again inside of the source tree
+python $SRC_DIR/setup.py build
 mkdir -p $PREFIX/share/jupyter/voila/templates/base/static
 cp $SRC_DIR/share/jupyter/voila/templates/base/static/*.map $PREFIX/share/jupyter/voila/templates/base/static
