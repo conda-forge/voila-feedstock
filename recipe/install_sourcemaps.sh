8803bb94317a4f944700a171985438005a0aeb8e
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-10-20 14:04:59 +0200

    just run npm

--- recipe/install_sourcemaps.sh
+++ recipe/install_sourcemaps.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 # build again inside of the source tree
-python $SRC_DIR/setup.py build
+(cd $SRC_DIR/js; npm install)
 mkdir -p $PREFIX/share/jupyter/voila/templates/base/static
 cp $SRC_DIR/share/jupyter/voila/templates/base/static/*.map $PREFIX/share/jupyter/voila/templates/base/static
